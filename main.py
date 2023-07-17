45dff7ce3ec9b36bc3e058a4713fbf6074f67206
jwhaney <johnthree@pm.me>
2023-07-17 17:03:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from renamer import rename
 # setup variables
 data_dir = 'DRIVE:\\PARCEL\\DATA\\LIVES\\HERE\\'
 out_workspace = 'parcel_update.gdb'
-parcels_sde = 'parcel_enterprise_gdb.sde'
 
 # run the repair function first which then calls the state_compiler
 run = floop(data_dir, out_workspace)
-
-# run rename function
-# r = rename(parcels_sde)
